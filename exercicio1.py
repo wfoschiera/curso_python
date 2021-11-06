13bf5a3b909ee7d13863a9cdab6cc9046df5a108
williamfoschiera <william.foschiera@buser.com.br>
2021-11-05 20:43:27 -0400

    criada a função de multiplicação

--- exercicio1.py
+++ exercicio1.py
@@ -1,9 +1,14 @@
 """
 Crie uma função que some dois numeros
-
+Crie outra função que multiplique dois numeros
 """
 def soma(a, b):
     return a + b
 
+def multiplicacao(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     assert soma(1, 2) == 3
+    assert multiplicacao(2, 3) == 6
