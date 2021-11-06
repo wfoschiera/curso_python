7332d61c641d366b3b964c8435a1a6ee2b7c599d
wfoschiera <wfoschiera@gmail.com>
2021-11-05 22:36:55 -0300

    info_dict funcionando

---
+++ youtube_video_info.py
@@ -0,0 +1,17 @@
+from youtube_dl import YoutubeDL
+import sys
+import time
+
+def baixar_info_video(video_id):
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+    youtube_dl_options = {}
+    with YoutubeDL(youtube_dl_options) as ydl:
+        info_dict = ydl.extract_info(video_url, download=False)
+        return info_dict
+    
+
+
+
+video1 = "uhG8UcjsKuo"
+
+info = baixar_info_video(video1)
